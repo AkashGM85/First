e795b3f3c59838bb36e88d2a61e4c7a3d4f649da
AkashGM85 <136160181+AkashGM85@users.noreply.github.com>
2023-06-10 14:50:41 +0530

    Add files via upload

---
+++ git.py
@@ -0,0 +1,47 @@
+def add(x, y):
+    """Addition function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication function"""
+    return x * y
+
+def divide(x, y):
+    """Division function"""
+    if y != 0:
+        return x / y
+    else:
+        print("Error: Cannot divide by zero")
+
+# Main program
+while True:
+    print("Calculator Menu:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+    
+    choice = input("Enter your choice (1-5): ")
+    
+    if choice == '5':
+        print("Exiting calculator...")
+        break
+    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input. Please enter a valid choice.")
